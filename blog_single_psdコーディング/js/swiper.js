e1f55492a150ebfb00e1ed4e30f76ebdb521a164
mai <65140855+mai-mogu@users.noreply.github.com>
2020-10-28 13:56:32 +0900

    Add files via upload
    ポートフォリオ4psdBlog

---
+++ blog_single_psdコーディング/js/swiper.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+});
